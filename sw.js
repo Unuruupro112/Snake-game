8fea5c730b1ed8ee2cf66fb813e44e6936a1cf47
Unuruupro112 <kakumiki9@gmail.com>
2026-04-24 00:23:00 +0700

    Update sw.js
    G

--- sw.js
+++ sw.js
@@ -1,15 +1,26 @@
+const CACHE_NAME = 'snake-game-v2'; // Энд v2 гэж өөрчиллөө
+
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Шинэ хувилбар гарвал шууд хүлээлгүйгээр идэвхжүүлнэ
   e.waitUntil(
-    caches.open('snake-store').then((cache) => {
-      return cache.addAll(['index.html']);
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html']))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) { // Хуучин хувилбарыг (v1) устгана
+          return caches.delete(key);
+        }
+      }));
     })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
